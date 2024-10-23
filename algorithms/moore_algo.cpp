5c3436b980aaff1c0c3771b97747265cc99fd1cd
xJBx1 <jairajbajaj02@gmail.com>
2024-10-24 10:14:21 +1100

    update in moore's voting algorithm

--- algorithms/moore_algo.cpp
+++ algorithms/moore_algo.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+/*
+This Algorithm is like a voting concept as it sees similar elements consecutively it will increase the vote,
+when it detects a different element in a new iteration of the loop it will reduce the vote. Once the vote 
+becomes 0, it will update the ans which was previously the majority element to a the new value it sees currently.
+The last value stored in ans is the majority element.
+
+The assumptions here is that there is a definitive majority element (n/2).
+*/
 int main()
 {
     int arr[9] = {1, 3, 5, 5, 3, 3, 1, 3, 3};
